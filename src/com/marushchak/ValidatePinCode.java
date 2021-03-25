ccf363f2631deeb6283dadb782fd1da6e7fbcff8
Artem1018 <66421506+Artem1018@users.noreply.github.com>
2021-03-25 09:34:50 +0200

    validatePinCode-complete

---
+++ src/com/marushchak/ValidatePinCode.java
@@ -0,0 +1,13 @@
+package com.marushchak;
+
+public class ValidatePinCode {
+    public static void main(String[] args) {
+        System.out.println(validatePin("1021"));
+    }
+    public static boolean validatePin(String pin) {
+        // Your code here...
+        if (pin.length() == 4 || pin.length() == 6)
+            return pin.chars().allMatch(Character::isDigit);
+        return false;
+    }
+}
