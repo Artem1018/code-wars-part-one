a983545531efb5cd4779e8f4167958df4093a764
Artem1018 <66421506+Artem1018@users.noreply.github.com>
2021-03-26 20:24:58 +0200

    StopGninnipSMySdroW

--- src/com/marushchak/StopGninnipSMySdroW.java
+++ src/com/marushchak/StopGninnipSMySdroW.java
@@ -1,5 +1,33 @@
 package com.marushchak;
 
-public class StopGninnipSMySdroW
-{
+public class StopGninnipSMySdroW {
+    public static void main(String[] args) {
+        System.out.println(spinWords("This is another test"));
+    }
+
+    public static String spinWords(String sentence) {
+        char[] charStr = sentence.toCharArray();
+        int letterCounter = 0;
+        sentence = "";
+        StringBuilder stringWord = new StringBuilder();
+        for (int i = 0; i < charStr.length; i++) {
+
+            if (charStr[i] == ' ' || i == charStr.length - 1){
+                if (i == charStr.length - 1)
+                    stringWord.append(charStr[i]);
+                if (letterCounter >= 5){
+                    stringWord.reverse();
+                }
+                sentence = sentence.concat(stringWord.toString() + " ");
+                letterCounter = 0;
+                stringWord = new StringBuilder();
+                continue;
+            }
+            stringWord.append(charStr[i]);
+            letterCounter++;
+        }
+        stringWord = new StringBuilder(sentence);
+        stringWord.deleteCharAt(stringWord.length() - 1);
+        return stringWord.toString();
+    }
 }
