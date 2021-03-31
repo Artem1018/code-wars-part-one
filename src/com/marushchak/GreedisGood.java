b35e7bfb55881aefe492df690589d0c050499a85
Artem1018 <66421506+Artem1018@users.noreply.github.com>
2021-03-31 19:52:21 +0300

    update code

--- src/com/marushchak/GreedisGood.java
+++ src/com/marushchak/GreedisGood.java
@@ -6,66 +6,12 @@ public class GreedisGood {
         System.out.println(greedy(new int[]{2, 4, 4, 5, 4}));
     }
 
-    public static int greedy(int[] dice){
-        int triplet =findThreeNumber(dice);
-        int score = 0;
-        if (triplet == 0)
-            score += findFiveOrOne(dice);
-            else{
-                if (triplet == 1)
-                score += triplet * 1000;
-                else
-                    score += triplet * 100;
-            dice = deleteThreeNumber(dice);
-            score += findFiveOrOne(dice);
+
+
+        public static int greedy(int[] dice) {
+            int n[] = new int[7];
+            for (int d : dice) n[d]++;
+            return n[1] / 3 * 1000 + n[1] % 3 * 100 + n[2] / 3 * 200 + n[3] / 3 * 300 + n[4] / 3 * 400 + n[5] / 3 * 500 + n[5] % 3 * 50 + n[6] / 3 * 600;
+
         }
-        return score;
-    }
-    public static int findFiveOrOne(int[] arr){
-            int score = 0;
-        for (int j : arr) {
-            if (j == 1) score += 100;
-            if (j == 5) score += 50;
-        }
-        return score;
-    }
-
-
-
-    public static int findThreeNumber(int[] num){
-        int counter = 1;
-        for (int i = 1; i < num.length; i++) {
-            if (num[i-1] == num[i])
-                counter++;
-            else
-                counter = 1;
-
-            if (counter==3)
-                return num[i];
-        }
-        return 0;
-    }
-    public static int[] deleteThreeNumber(int[] num){
-        int counter = 1;
-        int start = 0;
-        int end = 0;
-        for (int i = 1; i < num.length; i++) {
-            if (num[i-1] == num[i])
-                counter++;
-            else{
-                start = i;
-                counter = 1;
-            }
-
-
-            if (counter==3){
-                end = i+1;
-                break;
-            }
-        }
-        for (int i = start; i < end; i++) {
-           num[i] = 0;
-        }
-       return num;
-    }
 }
