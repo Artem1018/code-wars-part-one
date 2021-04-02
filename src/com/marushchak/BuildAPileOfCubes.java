1e7a2d8f0079289dfa3c644b95510c88f6f5f44e
Artem1018 <66421506+Artem1018@users.noreply.github.com>
2021-04-02 17:36:13 +0300

    add new solution

---
+++ src/com/marushchak/BuildAPileOfCubes.java
@@ -0,0 +1,12 @@
+package com.marushchak;
+
+public class BuildAPileOfCubes {
+    public static void main(String[] args) {
+        System.out.println(findNb(1071225));
+    }
+        public static long findNb(long m) {
+            long mm = 0, n = 0;
+            while (mm < m) mm += ++n * n * n;
+            return mm == m ? n : -1;
+        }
+}
